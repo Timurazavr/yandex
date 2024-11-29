f5234b8a491aa1078d038e4be87983ea999cf928
Timur <timur.garipov11@gmail.com>
2024-11-29 17:00:01 +0300

    Git и случайные окружности

--- main.py
+++ main.py
@@ -1,15 +1,42 @@
 import random
 import sys
 
-from PyQt6 import uic
+from PyQt6 import uic, QtCore, QtGui, QtWidgets
 from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtWidgets import QApplication, QMainWindow
 
 
-class MyWidget(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(603, 603)
+        self.centralwidget = QtWidgets.QWidget(parent=MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(parent=self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(220, 10, 131, 31))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(parent=MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 603, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(parent=MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Yellow circles"))
+        self.pushButton.setText(_translate("MainWindow", "Показать окружность"))
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("UI.ui", self)
+        self.setupUi(self)
         self.flag = False
         self.pushButton.clicked.connect(self.run)
 
@@ -17,16 +44,20 @@ class MyWidget(QMainWindow):
         if self.flag:
             qp = QPainter()
             qp.begin(self)
-            self.draw_yellow_circles(qp)
+            self.draw_rnd_circles(qp)
             qp.end()
 
     def run(self):
         self.flag = True
         self.repaint()
 
-    def draw_yellow_circles(self, qp):
+    def draw_rnd_circles(self, qp):
         x = random.randint(110, 450)
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(
+            QColor(
+                random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+            )
+        )
         qp.drawEllipse(100, 100, x, x)
 
 
